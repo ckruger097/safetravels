4d34b632743913fa35f02f38c3e3662d53b37ca7
Andrew Babilonia <86068252+FLAIVIINGO@users.noreply.github.com>
2021-07-02 07:28:49 -0400

    updated search bar functionality.

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from os import environ
 from dotenv import load_dotenv
 from datetime import datetime as dt
 from jinja2 import Environment
-import dns
+import dns, requests
 
 app = Flask(__name__, template_folder='./app/templates', static_folder='./app/static')
 app.config['SECRET_KEY'] = 'I figure if I study high, take the test high, get high scores! Right?'
@@ -43,6 +43,117 @@ def hello_mongo():
     for doc in result:
         return str(doc.get("string"))
 
+
+# Search page, not yet integrated with the JS search bar
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    if request.method == "POST":
+        req = request.form
+        state = req.get("state").lower()  
+        if state in us_state_abbrev:
+            abbrev = us_state_abbrev.get(state)      
+            return redirect(f"/us/{state}-{abbrev}")
+    return render_template("search.html")
+
+def api_handler():
+    covid_api = 'ad85ef1a0e9b4ad4aecc0ffe0792549e'
+    url = 'https://api.covidactnow.org/v2/states.json?apiKey=' + covid_api
+    data = requests.get(url)
+    state_list = data.json()
+    return state_list
+
+@app.route("/us")
+def us_page():
+    state_list = api_handler()
+    headings = ("State", "Cases", "Deaths", "Vaccinations Completed")
+    output = []
+
+    for state in state_list:
+        metrics = (f"{state.get('state')}", f"{state.get('actuals').get('cases')}", f"{state.get('actuals').get('deaths')}",
+        f"{state.get('actuals').get('vaccinationsCompleted')}")
+        output.append(metrics)
+
+    return render_template("country.html", headings=headings, output=output)
+
+@app.route("/us/<state>-<abbrev>")
+def state(state, abbrev):
+    in_state = state
+    in_abbrev = abbrev
+    
+    
+    states = api_handler()
+    abbrev = abbrev.upper()
+    
+    headings = ("State", "Cases", "Deaths", "Vaccinations")
+
+    for state in states:
+        if state.get('state') == abbrev:
+            case_numbers = state.get('actuals').get('cases')
+            deaths = state.get('actuals').get('deaths')
+            vaccine = state.get('actuals').get('vaccinationsCompleted')
+            break
+
+    return render_template("state.html", in_state=in_state, in_abbrev=in_abbrev, case_numbers=case_numbers, headings=headings
+    , deaths=deaths, vaccine=vaccine)
+
+us_state_abbrev = {
+    'alabama': 'al',
+    'alaska': 'ak',
+    'american samoa': 'as',
+    'arizona': 'az',
+    'arkansas': 'ar',
+    'california': 'ca',
+    'colorado': 'co',
+    'connecticut': 'ct',
+    'delaware': 'de',
+    'district of columbia': 'dc',
+    'florida': 'fl',
+    'georgia': 'ga',
+    'guam': 'gu',
+    'hawaii': 'hi',
+    'idaho': 'id',
+    'illinois': 'il',
+    'indiana': 'in',
+    'iowa': 'ia',
+    'kansas': 'ks',
+    'kentucky': 'ky',
+    'louisiana': 'la',
+    'maine': 'me',
+    'maryland': 'md',
+    'massachusetts': 'ma',
+    'michigan': 'mi',
+    'minnesota': 'mn',
+    'mississippi': 'ms',
+    'missouri': 'mo',
+    'montana': 'mt',
+    'nebraska': 'ne',
+    'nevada': 'nv',
+    'new hampshire': 'nh',
+    'new jersey': 'nj',
+    'new mexico': 'nm',
+    'new york': 'ny',
+    'north carolina': 'nc',
+    'north dakota': 'nd',
+    'northern mariana islands':'mp',
+    'ohio': 'oh',
+    'oklahoma': 'ok',
+    'oregon': 'or',
+    'pennsylvania': 'pa',
+    'puerto rico': 'pr',
+    'rhode island': 'ri',
+    'south carolina': 'sc',
+    'south dakota': 'sd',
+    'tennessee': 'tn',
+    'texas': 'tx',
+    'utah': 'ut',
+    'vermont': 'vt',
+    'virgin islands': 'vi',
+    'virginia': 'va',
+    'washington': 'wa',
+    'west virginia': 'wv',
+    'wisconsin': 'wi',
+    'wyoming': 'wy'
+}
 
 # Example of templating
 @app.route('/index')
